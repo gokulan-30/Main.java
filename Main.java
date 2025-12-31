79d6dd3f85f7987a05232f99a2e42fc92612b3d6
Gokulan S <167771108+gokulan-30@users.noreply.github.com>
2025-12-31 08:28:29 +0530

    Add Main.java file

---
+++ Main.java
@@ -0,0 +1,7 @@
+# Main.java
+Banking Management Capstone using Java
+public class Main{
+  public static void main(String[]args){
+    System.out.println("Hello World");
+  }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Main.java
-Banking Management Capstone using Java
